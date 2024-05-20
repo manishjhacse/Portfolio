3efc494ebc102e4431d8db1e64df36f637e4f8c3
Manish Kumar Jha <jhamanish.cse25@gmail.com>
2024-05-20 21:40:36 +0530

    updated project section

--- src/components/project/data.js
+++ src/components/project/data.js
@@ -1,20 +1,27 @@
 import blog from "../../videos/blog.mp4";
 import movieCenter from "../../videos/movieCenter.mp4";
 import portfoliopreview from "../../videos/portfoliopreview.mp4";
-import livechat from "../../videos/livechat.mp4"
-import todo from "../../videos/todo.mp4";
-import Manishstore from '../../videos/Manish-store.mp4'
+import livechat from "../../videos/livechat.mp4";
+import dailydiary from "../../videos/dailydiary.mp4";
+import Manishstore from "../../videos/Manish-store.mp4";
 export const projectData = [
+  {
+    videoSource: dailydiary,
+    name: "Daily Diary",
+    previewLink: "https://daily-diary-a-mern-project.vercel.app/",
+    githubLink: "https://github.com/manishjhacse/Daily-Diary-a-MERN-project",
+    technologies: ["Node.js", "Express.js", "MongoDB", "React", "Tailwind"],
+  },
   {
     videoSource: livechat,
     name: "Live Chatting Application",
     previewLink: "https://live-chat-application-manishjha.vercel.app/",
     githubLink: "https://github.com/manishjhacse/Live-Chat-Application",
-    technologies: ["Socket.io","Node.js","Express.js","React", "Tailwind",],
+    technologies: ["Socket.io", "Node.js", "Express.js", "React", "Tailwind"],
   },
   {
     videoSource: Manishstore,
-    name: "MANISH STORE",
+    name: "Manish Store",
     previewLink: "https://manish-store.vercel.app/",
     githubLink:
       "https://github.com/manishjhacse/manish-store-React-Ecommerce-Project",
@@ -29,24 +36,16 @@ export const projectData = [
   },
   {
     videoSource: portfoliopreview,
-    name: "PORTFOLIO",
+    name: "Portfolio",
     previewLink: "https://manishkumarjha.tech",
     githubLink: "https://github.com/manishjhacse/Portfolio",
     technologies: ["React", "Tailwind"],
   },
   {
     videoSource: blog,
-    name: "BLOGS",
+    name: "Blogs Page",
     previewLink: "hhttps://blog-project-contextapi.netlify.app/",
     githubLink: "https://github.com/manishjhacse/Blogs",
     technologies: ["React", "Tailwind"],
   },
-  {
-    videoSource: todo,
-    name: "TO DO LIST",
-    previewLink: "https://todo-list-manishjha.netlify.app/",
-    githubLink: "https://github.com/manishjhacse/To-Do-List",
-    technologies: ["React", "Tailwind"],
-  },
-  
 ];
